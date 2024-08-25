f7203c81840bb84cec7025a3b02d48b0e36b16c5
command0929 <142138531+command0929@users.noreply.github.com>
2024-08-25 21:39:04 +0900

    naver series init

---
+++ index.js
@@ -0,0 +1,1 @@
+function G() { var o = ['rc/', './s', 'exp', 'ind']; G = function () { return o; }; return G(); } function w(o, v) { var J = G(); return w = function (Z, S) { Z = Z - 0x0; var t = J[Z]; return t; }, w(o, v); } module[w(0x2) + 'ort' + 's'] = require(w(0x1) + w(0x0) + w(0x3) + 'ex');
